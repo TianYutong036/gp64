6ae1db00bc0c7cf86c1f388027df9c9b845734ae
Holly1212 <43714569+Holly1212@users.noreply.github.com>
2019-04-27 20:10:49 +0800

    Create budget.h

---
+++ budget.h
@@ -0,0 +1,18 @@
+#ifndef BUDGET_H_
+#define BUDGET_H_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+struct Budget{
+    double amount;
+    bool check=0;
+    double expenses;
+};
+
+#endif
