8ac45733a895379d8749e86e33b1d3c86c4a0e3f
Holly1212 <43714569+Holly1212@users.noreply.github.com>
2019-04-27 20:10:47 +0800

    Create account.h

---
+++ account.h
@@ -0,0 +1,15 @@
+#ifndef ACCOUNT_H_
+#define ACCOUNT_H_
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+struct Account{
+  string name;
+  double balance;
+};
+
+#endif
